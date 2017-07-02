dd318ffaf6f94a04471c23265acc42b3777e7a36
lgeffect <lgeffect@yandex.ru>
2017-07-02 16:25:04 +0300

    Functional template

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
 
 def post_list(req):
-    return render(req, 'blogapp/post_list.html', {})
+    posts = Post.objects.filter(publish_date__lte=timezone.now()).order_by('publish_date')
+    return render(req, 'blogapp/post_list.html', {'posts' : posts})
